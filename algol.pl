f8e0362c3c3964d0dd112d2685919441139d2607
Bixkog <wojciechoziebly@gmail.com>
2016-05-04 22:12:20 +0200

    only labels left

--- algol.pl
+++ algol.pl
@@ -113,11 +113,8 @@ parser(program(EV,EF,I)) --> [tProgram],[_], block(EV,EF,I).
 block(EV,EF,I) --> declarations(EV,EF), [tBegin], instructions(I), [tEnd].
 % deklaracje-------------------------------------------------------------
 declarations(EV,[F|EF]) --> procDecl(F),!,declarations(EV,EF).
-declarations(EV,EF) --> varDecl(EV),!, fDeclarations(EF).
+declarations(EV,EF) --> varDecl(V),!, declarations(EV2,EF),{append(V,EV2,EV)}.
 declarations([],[]) --> [].
-
-fDeclarations([F|EF]) --> procDecl(F),!,fDeclarations(EF).
-fDeclarations([]) --> [].
 
 varDecl([V|EV]) --> [tLocal],[tVar(Id)],{V = var(Id,Adr)}, cvarDecl(EV).
 cvarDecl([V|EV]) --> [tColon],!, [tVar(Id)],{V = var(Id,Adr)} , cvarDecl(EV).
@@ -174,10 +171,10 @@ atomicExpr(S) --> (
     [tVar(Id)],!, {S = var(Id)};
     [tNumber(S)]).
 
-addtiveOp(plus) --> [tPlus],!.
-addtiveOp(minus) --> [tMinus].
+addtiveOp(add) --> [tPlus],!.
+addtiveOp(sub) --> [tMinus].
 
-multOp(mult) --> [tTimes],!.
+multOp(mul) --> [tTimes],!.
 multOp(div) --> [tDiv],!.
 multOp(mod) --> [tMod].
 % wolanie-funkcji---------------------------------------------------------
@@ -225,6 +222,13 @@ get_label_id(X):-
 	N is X + 1,
 	b_setval(counter,N).
 
+makro_compile([],_,_,[]).
+makro_compile([I|IS],EV,EF,Makro):-
+	makro_compile_instruction(I,EV,EF,Makro_I),
+	makro_compile(IS,EV,EF,Makro_IS),
+	append(Makro_I,Makro_IS,Makro).
+
+
 
 makro_compile_instruction(assign(var(LValueId),expr(RValueId)),EV,EF,Makro):-!,
 	memberchk(var(LValueId,LVAddr),EV),
@@ -255,6 +259,10 @@ makro_compile_instruction(while(Condition,Do),EV,EF,Makro):-!,
 	       [branchn(End_id)]
 	       ,Makro_Do,
 	       [jump(Start_id),label(End_id)]],Makro).
+makro_compile_instruction(return(expr(E)),EV,EF,Makro):-!,
+	makro_compile_expression(E,EV,EF,Makro).
+
+
 %makro_compile_instruction(funCall(Id,Args),EV,EF,Makro):-
 %makro_compile_instruction(return(expr(E)),EV,EF,Makro):-
 %%	write: ACC --> O
@@ -269,6 +277,8 @@ makro_compile_instruction(write(expr(E)),EV,EF,Makro):-
 makro_compile_expression(E,EV,EF,Makro_expr):-
 	makro_compile_expression(0,E,EV,EF,Makro_expr).
 
+makro_compile_expression(N, funCall(Id,Args),EV,EF,Makro_expr):-!,
+	call_function(N,funCall(Id,Args),EV,EF,Makro_expr).
 makro_compile_expression(N,E,EV,EF,Makro_expr):-
 	E =.. [Op, E1, E2],!,
 	makro_compile_expression(N,E1,EV,EF,Makro_expr1),
@@ -278,8 +288,6 @@ makro_compile_expression(N,E,EV,EF,Makro_expr):-
 makro_compile_expression(N,var(X),EV,EF,Makro_expr):-!,
 	memberchk(var(X,Addr),EV),
 	Makro_expr = [load(Addr)].
-makro_compile_expression(N, funCall(Id,Args),EV,EF,Makro_expr):-!,
-	call_function(N,funCall(Id,Args),EV,EF,Makro_expr).
 makro_compile_expression(N, Number, EV, EF, [const(Number)]).
 
 %%	m_c_condition: Condition -> ACC = 0 if true ; ACC < 0 if false
@@ -332,15 +340,112 @@ makro_compile_condition(R_Cond, EV, EF, Makro_Condition):-
 	;   Op == leq, Swap = true, get_label_id(End_id),
 	    Jumps = [branchn(End_id),const(0),label(End_id)]),
 	(   Swap == false,!,
-	    concat([Makro_expr1, [push],Makro_expr2,[swapd,pop,minus]],Start)
-	;   concat([Makro_expr2, [push],Makro_expr1,[swapd,pop,minus]],Start)),
+	    concat([Makro_expr1, [push],Makro_expr2,[swapd,pop,sub]],Start)
+	;   concat([Makro_expr2, [push],Makro_expr1,[swapd,pop,sub]],Start)),
+
 	concat([Start,Jumps],Makro_Condition).
 
 
+%% push: ACC -> stack
+assembly_makro_instruction(push,Makro):-!,
+	Makro = [swapd,const(65535),swapa, load, swapa, swapd, store,
+		 const(1), swapd, swapa, sub, store].
+%% pop: stack -> ACC, saves DR
+assembly_makro_instruction(pop,Makro):-!,
+	Makro = [const(65535),swapa,load,swapd,swapa,const(1),swapd,
+		 sub,swapa,swapd,const(65535),swapa,store,swapa,load].
+
+assembly_makro_instruction(load(Addr),Makro):-!,
+	Makro = [const(Addr),swapa,load].
+
+assembly_makro_instruction(store(Addr),Makro):-!,
+	Makro = [swapa,const(Addr),swapa,store].
+%saves ACC, DR
+assembly_makro_instruction(branchn(Label_id),Makro):-!,
+	Makro = [swapa,const(label(Label_id)),swapa,branchn].
+%saves ACC, DR
+assembly_makro_instruction(branchz(Label_id),Makro):-!,
+	Makro = [swapa,const(label(Label_id)),swapa,branchz].
+%saves ACC, DR
+assembly_makro_instruction(jump(Label_id),Makro):-!,
+	Makro = [swapa,const(label(Label_id)),swapa,jump].
+% ACC mod DR -> ACC
+assembly_makro_instruction(mod,Makro):-
+	Makro = [div,const(-16),swapd,shift].
+
+first_assembly([],[]):-!.
+first_assembly([M|Makros],FASSEMBLY):-
+	(assembly_makro_instruction(M,Assembly_inst)
+	;Assembly_inst = [M]),
+	first_assembly(Makros,FArest),
+	append(Assembly_inst,FArest,FASSEMBLY).
+
+pack_length([],0):-!.
+pack_length([label(_)|L],N):-!,
+	pack_length(L,N).
+pack_length([_|L],N):-
+	pack_length(L,N2),
+	N is N2	+ 1.
+
+generate_NOPS(0,[]):-!.
+generate_NOPS(N,[nop|NOPS]):-
+	N2 is N-1,
+	generate_NOPS(N2,NOPS).
+
+fill_with_NOPS(L,R):-
+	pack_length(L,N),
+	NOP_C is 4-N,
+	generate_NOPS(NOP_C,NOPS),
+	append(L,NOPS,R).
+
+pack(AIS,P,[P|L]):-
+	pack_length(P,4),!,
+	pack(AIS,[],L).
+
+pack([label(Id)|AIS],[],[label(Id)|L]):-!,
+	pack(AIS,[],L).
+pack([label(Id)|AIS],P,[P2,label(Id)|L]):-!,
+	fill_with_NOPS(P,P2),
+	pack(AIS,[],L).
+
+pack([AI|AIS],P,[P2|L]):-
+	memberchk(AI,[jump,branchn,branchz]),!,
+	append(P,[AI],P1),
+	fill_with_NOPS(P1,P2),
+	pack(AIS,[],L).
+
+pack([AI|AIS],P,L):-
+	append(P,[AI],P2),!,
+	pack(AIS,P2,L).
+
+pack([],P,[P]).
 
 
+dec_to_machine_word(N,MW):-
+	N>=0,!,
+	MW = N.
+dec_to_machine_word(N,MW):-
+	MW is 65536+N.
+
+assembly_const([],[]).
+assembly_const([label(Id)|L],[label(Id)|R]):-!,
+	assembly_const(L,R).
+assembly_const([H|L],[E|R]):-
+	get_consts(H,Cs,E),
+	Cs == [],!,
+	assembly_const(L,R).
+assembly_const([H|L],[E,Cs|R]):-
+	get_consts(H,Cs,E),
+	assembly_const(L,R).
 
 
+get_consts([],[],[]).
+get_consts([const(C)|L],[C|R],[const|E]):-!,
+	get_consts(L,R,E).
+get_consts([H|L],R,[H|E]):-
+	get_consts(L,R,E).
 
+set_labels(I,R,ML):-
+	set_labels(I,R,ML,0).
 
 
